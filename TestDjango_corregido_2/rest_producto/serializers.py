2741a3b4d6626ca2e4d7e300623b20689c8fc36b
josevalpo <jose.valpo@gmail.com>
2024-07-12 16:36:49 -0400

    Carga inicial

--- TestDjango_corregido_2/rest_producto/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from core.models import Producto
-class ProductoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producto
-        fields =['sku', 'nombre', 'precio', 'stock', 'descripcion', 'categoria']
